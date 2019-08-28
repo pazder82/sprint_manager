d69397a5c74641afbf0bd55512848b9f0043ebe4
Jan Pazdera <jan.pazdera@flowmon.com>
2019-08-28 21:34:07 +0200

    Filters fully working.

--- lib/patches/query_patch.rb
+++ lib/patches/query_patch.rb
@@ -24,11 +24,12 @@ module SprintTeams
 
       module InstanceMethods
         def statement_with_sprint_teams
-          filter  = filters.delete 'issue_teams'
+          filter_teams  = filters.delete 'issue_teams'
+          filter_sprints  = filters.delete 'issue_sprints'
           clauses = statement_without_sprint_teams || ''
 
-          if filter
-            filters['issue_teams'] = filter
+          if filter_teams
+            filters['issue_teams'] = filter_teams
             op = operator_for('issue_teams')
             if op.include?('!')
               compare = 'NOT IN'
@@ -37,7 +38,6 @@ module SprintTeams
               compare = 'IN'
               empty_ids_clause = 'FALSE'
             end
-
             clauses << ' AND ' unless clauses.empty?
             issue_ids = IssueTeamsDatum.where(teams_id: values_for('issue_teams')) # find issue ids with this team assigned
             if issue_ids.any?
@@ -46,9 +46,45 @@ module SprintTeams
               # no issue ids found for this team
               clauses << empty_ids_clause
             end
-            #binding.pry
           end
 
+          if filter_sprints
+            filters['issue_sprints'] = filter_sprints
+            issue_ids = ''
+            sprint_values = values_for('issue_sprints')
+            op = operator_for('issue_sprints')
+
+            if sprint_values.any?
+              if op == '='
+                #is
+                issue_ids = IssueTeamsDatum.where(sprint: sprint_values[0]) # find issue ids with this sprint assigned
+              elsif op == '>='
+                #>=
+                issue_ids = IssueTeamsDatum.where('sprint >= ?', sprint_values[0])
+              elsif op == '<='
+                #<=
+                issue_ids = IssueTeamsDatum.where('sprint <= ?', sprint_values[0])
+              elsif op == '><'
+                #between
+                issue_ids = IssueTeamsDatum.where('sprint > ?', sprint_values[0]).where('sprint < ?', sprint_values[1])
+              elsif op == '!*'
+                #none
+                issue_ids = IssueTeamsDatum.where("sprint IS NULL")
+              else
+                #any
+                issue_ids = IssueTeamsDatum.where("sprint IS NOT NULL")
+              end
+            end
+
+            clauses << ' AND ' unless clauses.empty?
+            if issue_ids.any?
+              clauses << "( #{Issue.table_name}.id IN (#{issue_ids.map(&:issues_id).join(',')}) ) "
+            else
+              # no issue ids found for this team
+              clauses << 'FALSE'
+            end
+            #binding.pry
+          end
           clauses
         end
 
@@ -56,7 +92,7 @@ module SprintTeams
           available_filters_without_sprint_teams
           teams_list = Team.all.map { |c| [ c.team, c.id.to_s ] }
           add_available_filter('issue_teams', type: :list, name: l(:filter_team), values: teams_list)
-          add_available_filter('issue_sprint', type: :integer, name: l(:filter_sprint))
+          add_available_filter('issue_sprints', type: :integer, name: l(:filter_sprint))
         end
       end
     end
