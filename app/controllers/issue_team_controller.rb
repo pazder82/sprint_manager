3654c5d10079dbce6b5e57c5a6165b42fe4f6570
Jan Pazdera <jan.pazdera@flowmon.com>
2019-11-11 22:52:03 +0100

    Context menu working for teams. Not yet working for sprints. Page refresh not working.

--- app/controllers/issue_team_controller.rb
+++ app/controllers/issue_team_controller.rb
@@ -1,29 +1,37 @@
 class IssueTeamController < ApplicationController
   unloadable
 
-  before_action :find_issues, :only => [:edit, :update]
+  before_action :find_issues, :only => [:edit]
 
   def edit
     @issue_ids = params[:ids]
-    @is_bulk_editing = @issue_ids.size > 1
-    @team_id = params[:team_id]
-  end
-
-  def update
-    # FIXME:
-    team = params[:issue]
-
-    Issue.transaction do
-      @issues.each do |issue|
-        #issue.team = team
-        issue.save!
+    @team_id = params[:team_id] if params[:team_id]
+    @sprint = params[:sprint] if params[:sprint]
+    @issue_ids.each do |issue_id|
+      issue_teams_datum = IssueTeamsDatum.find_by('issue_id' => issue_id)
+      if @team_id
+        if !issue_teams_datum
+          # Not yet existing in table - create new row
+          issue_teams_datum = IssueTeamsDatum.new
+          issue_teams_datum[:issue_id] = issue_id
+        end
+        issue_teams_datum[:team_id] = @team_id
+      end
+      if issue_teams_datum
+        if @sprint == "current"
+          issue_teams_datum[:sprint] = getTeamSprint(issue_teams_datum[:team_id])
+        elsif @sprint == "next"
+          issue_teams_datum[:sprint] = getTeamSprint(issue_teams_datum[:team_id])+1
+        end
+        issue_teams_datum.save
       end
     end
-    flash[:notice] = t(:notice_tags_added)
-  rescue Exception => e
-    puts e
-    flash[:error] = t(:notice_failed_to_add_tags)
-  ensure
-    redirect_to_referer_or { render :text => 'Tags updated.', :layout => true }
+    redirect_to :back
   end
+
+  def getTeamSprint(team_id)
+    # FIXME:
+    11
+  end
+
 end
